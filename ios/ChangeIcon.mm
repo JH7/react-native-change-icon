d1a61a9fafa76a662368d4f404f93393f0e31a85
Jonas Hillebrand <8239699+JH7@users.noreply.github.com>
2025-08-04 18:48:27 +0200

    fix(ios): protocol

--- ios/ChangeIcon.mm
+++ ios/ChangeIcon.mm
@@ -12,86 +12,82 @@
 }
 #endif
 
-- (void)getIcon:(RCTPromiseResolveBlock)resolve
-       rejecter:(RCTPromiseRejectBlock)reject {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        NSString *currentIconName = [[UIApplication sharedApplication] alternateIconName];
-        if (currentIconName) {
-            NSString *currentIcon = [currentIconName stringByReplacingOccurrencesOfString:@"icon-" withString:@""];
-            resolve(currentIcon);
-        } else {
-            resolve(@"standard");
-        }
-    });
-}
+- (void)changeIcon:(nonnull NSString *)iconName resolve:(nonnull RCTPromiseResolveBlock)resolve reject:(nonnull RCTPromiseRejectBlock)reject {
+  dispatch_async(dispatch_get_main_queue(), ^{
+      NSString *iconNameTogether = iconName ? [@"icon-" stringByAppendingString:iconName] : nil;
+    
+      bool useUnsafeSuppressAlert = true;
+      NSError *error = nil;
 
-- (void)resetIcon:(RCTPromiseResolveBlock)resolve
-         rejecter:(RCTPromiseRejectBlock)reject {
-    [self changeIcon:nil resolver:resolve rejecter:reject];
-}
+      if ([[UIApplication sharedApplication] supportsAlternateIcons] == NO) {
+          reject(@"Error", @"IOS:NOT_SUPPORTED", error);
+          return;
+      }
 
-- (void)changeIcon:(NSString *)iconName
-          resolver:(RCTPromiseResolveBlock)resolve
-          rejecter:(RCTPromiseRejectBlock)reject {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        NSString *iconNameTogether = iconName ? [@"icon-" stringByAppendingString:iconName] : nil;
+      NSString *currentIcon = [[UIApplication sharedApplication] alternateIconName];
+
+      if ([iconNameTogether isEqualToString:currentIcon] || (iconNameTogether == nil && currentIcon == nil)) {
+          reject(@"Error", @"IOS:ICON_ALREADY_USED", error);
+          return;
+      }
+
+      NSString *newIconName;
+      if (iconName == nil || [iconName length] == 0 || [iconName isEqualToString:@"Default"]) {
+          newIconName = nil;
+      } else {
+          newIconName = iconNameTogether;
+      }
       
-        bool useUnsafeSuppressAlert = true;
-        NSError *error = nil;
-
-        if ([[UIApplication sharedApplication] supportsAlternateIcons] == NO) {
-            reject(@"Error", @"IOS:NOT_SUPPORTED", error);
-            return;
-        }
-
-        NSString *currentIcon = [[UIApplication sharedApplication] alternateIconName];
-
-        if ([iconNameTogether isEqualToString:currentIcon] || (iconNameTogether == nil && currentIcon == nil)) {
-            reject(@"Error", @"IOS:ICON_ALREADY_USED", error);
-            return;
-        }
-
-        NSString *newIconName;
-        if (iconName == nil || [iconName length] == 0 || [iconName isEqualToString:@"Default"]) {
-            newIconName = nil;
-        } else {
-            newIconName = iconNameTogether;
-        }
-        
-        if (useUnsafeSuppressAlert == true) {
-            @try {
-                typedef void (*setAlternateIconName)(NSObject *, SEL, NSString *, void (^)(NSError *));
-                NSString *selectorString = @"_setAlternateIconName:completionHandler:";
-                SEL selector = NSSelectorFromString(selectorString);
-                IMP imp = [[UIApplication sharedApplication] methodForSelector:selector];
-                setAlternateIconName method = (setAlternateIconName)imp;
-                method([UIApplication sharedApplication], selector, newIconName, ^(NSError *error) {
-                    if (error != nil) {
-                        reject(@"Error", @"IOS:UNSAFE_ERROR", error);
-                    } else {
-                        resolve(newIconName ?: @"standard");
-                    }
-                });
-            }
-            @catch (NSException *exception) {
-              [[UIApplication sharedApplication] setAlternateIconName:newIconName completionHandler:^(NSError * _Nullable error) {
-                if (error != nil) {
-                    reject(@"Error", @"IOS:SAFE_ERROR", error);
-                } else {
-                    resolve(newIconName ?: @"standard");
-                }
-              }];
-            }
-        } else {
-          [[UIApplication sharedApplication] setAlternateIconName:newIconName completionHandler:^(NSError * _Nullable error) {
+      if (useUnsafeSuppressAlert == true) {
+          @try {
+              typedef void (*setAlternateIconName)(NSObject *, SEL, NSString *, void (^)(NSError *));
+              NSString *selectorString = @"_setAlternateIconName:completionHandler:";
+              SEL selector = NSSelectorFromString(selectorString);
+              IMP imp = [[UIApplication sharedApplication] methodForSelector:selector];
+              setAlternateIconName method = (setAlternateIconName)imp;
+              method([UIApplication sharedApplication], selector, newIconName, ^(NSError *error) {
+                  if (error != nil) {
+                      reject(@"Error", @"IOS:UNSAFE_ERROR", error);
+                  } else {
+                      resolve(newIconName ?: @"standard");
+                  }
+              });
+          }
+          @catch (NSException *exception) {
+            [[UIApplication sharedApplication] setAlternateIconName:newIconName completionHandler:^(NSError * _Nullable error) {
               if (error != nil) {
-                    reject(@"Error", @"IOS:ERROR", error);
-                } else {
-                    resolve(newIconName ?: @"standard");
-                }
-          }];
-        }
-    });
+                  reject(@"Error", @"IOS:SAFE_ERROR", error);
+              } else {
+                  resolve(newIconName ?: @"standard");
+              }
+            }];
+          }
+      } else {
+        [[UIApplication sharedApplication] setAlternateIconName:newIconName completionHandler:^(NSError * _Nullable error) {
+            if (error != nil) {
+                  reject(@"Error", @"IOS:ERROR", error);
+              } else {
+                  resolve(newIconName ?: @"standard");
+              }
+        }];
+      }
+  });
+}
+
+- (void)getIcon:(nonnull RCTPromiseResolveBlock)resolve reject:(nonnull RCTPromiseRejectBlock)reject {
+  dispatch_async(dispatch_get_main_queue(), ^{
+      NSString *currentIconName = [[UIApplication sharedApplication] alternateIconName];
+      if (currentIconName) {
+          NSString *currentIcon = [currentIconName stringByReplacingOccurrencesOfString:@"icon-" withString:@""];
+          resolve(currentIcon);
+      } else {
+          resolve(@"standard");
+      }
+  });
+}
+
+- (void)resetIcon:(nonnull RCTPromiseResolveBlock)resolve reject:(nonnull RCTPromiseRejectBlock)reject {
+  [self changeIcon:nil resolve:resolve reject:reject];
 }
 
 + (NSString *)moduleName
